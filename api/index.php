dd117eac58d73d79832c67bd5953e5efd6b0e8aa
yahya-nasser-eddine-daraane <yahyadaraan@gmail.com>
2026-04-27 03:06:58 +0100

    fix: definitive fix for Vercel 500 error by moving bootstrap logic and setting tmp paths

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+use Illuminate\Http\Request;
+
+$app->handleRequest(Request::capture());
