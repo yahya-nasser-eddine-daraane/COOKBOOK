f503f86dc0faccb807e1cba7ea823860912bc711
yahya-nasser-eddine-daraane <yahyadaraan@gmail.com>
2026-05-03 19:38:55 +0100

    fix laravel 11 cache envs

--- api/index.php
+++ api/index.php
@@ -16,7 +16,13 @@ require $basePath . '/vendor/autoload.php';
 
 $app = require_once $basePath . '/bootstrap/app.php';
 
+// Redirect Laravel's cache files to /tmp so they aren't read from the stale build folder
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+
 $app->useStoragePath('/tmp/storage');
-$app->useBootstrapPath('/tmp/bootstrap');
 
 $app->handleRequest(Request::capture());
